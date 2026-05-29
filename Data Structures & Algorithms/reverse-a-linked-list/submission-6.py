0af4b8f3470bed689fc7194d82a00775590daa61
kalavathimlops-hash <kalavathimlops@gmail.com>
2026-05-29 18:41:01 -0500

    Add: reverse-a-linked-list - submission-6

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-6.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseList(self,head:ListNode)->ListNode:
+        prev,curr=None,head
+        while curr:
+            temp=curr.next
+            curr.next=prev
+            curr=prev
+            curr=temp
+        return prev    
