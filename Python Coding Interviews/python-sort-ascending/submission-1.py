0ef89ff46d437498f5ed09282189a01cb07fba8c
kalavathimlops <kalavathimlops@gmail.com>
2026-08-12 11:24:32 -0500

    Add: python-sort-ascending - submission-1

---
+++ Python Coding Interviews/python-sort-ascending/submission-1.py
@@ -0,0 +1,12 @@
+from typing import List
+
+def sort_words(words:List[str]) ->List[str]:
+     words.sort()
+     return words
+def sort_numbers(numbers:List[int]) ->List[int]:
+    return numbers.sort()
+def sort_decimals(decimals:List[float])->List[float]:
+    return decimals.sort()
+print(sort_words(["cherry", "apple", "blueberry", "banana", "watermelon", "zucchini", "kiwi", "pear"]))
+print(sort_numbers([1, 5, 3, 2, 4, 11, 19, 9, 2, 5, 6, 7, 4, 2, 6]))
+print(sort_decimals([3.14, 2.82, 6.433, 7.9, 21.555, 21.554]))
